6350e844f50daeaf520e825d82ea207af2a8b0c5
EraserChiu612 <eraser0612@gmail.com>
2024-04-02 16:21:26 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { RouterView } from 'vue-router'
+
 const goList = () => {
   console.log('goList')
 }
@@ -9,13 +11,19 @@ const userStore = useUserStore()
 </script>
 
 <template>
-  <div>我是app</div>
-  <el-button>跳首頁</el-button>
-  <el-button @click="goList">跳列表頁</el-button>
+  <div>
+    <hr />
+    <router-view></router-view>
+    <hr />
+    <hr />
+    <hr />
+    <el-button>跳首頁</el-button>
+    <el-button @click="goList">跳列表頁</el-button>
 
-  <p>{{ userStore.token }}</p>
-  <el-button @click="userStore.setToken('qwregdsvxdghbersg')">登入</el-button>
-  <el-button @click="userStore.removeToken()">登出</el-button>
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('qwregdsvxdghbersg')">登入</el-button>
+    <el-button @click="userStore.removeToken()">登出</el-button>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/views/layout/LayoutContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    布局架子
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
